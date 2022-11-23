6f303cf727e8258eeb726e602c5fbee709e3d17b
Jordan Hollinger <jordan.hollinger@wellsky.com>
2022-11-23 13:24:46 -0500

    Add optional ActiveRecord::Base.schema_format = :sql

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -4,3 +4,6 @@ else
   OTR::ActiveRecord.configure_from_file! 'config/database.yml'
 end
 OTR::ActiveRecord.establish_connection!
+
+# Uncomment if you want to use structure.sql instead of schema.rb
+#ActiveRecord::Base.schema_format = :sql
